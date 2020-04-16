7afbd452af6f7de1ff64f80ce1fd0ff93b203b5b
Tobi <tobias.eisenschenk@nui.care>
2020-04-16 22:22:25 +0200

    initial commit

---
+++ create-stack.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name UdagramCFStack --template-body file://udagram-cf.yml --parameters file://params.json --capabilities CAPABILITY_NAMED_IAM --region us-east-2
---
+++ update-network-stack.sh
@@ -0,0 +1,1 @@
+aws cloudformation update-stack --stack-name UdagramNetworkingCFStack --template-body file://networking-cf.yml --region us-east-2
